b7c210f7730756ec5ae80407bf8f2cc536df276e
Kamran <Yousef.shaikhan@gmail.com>
2025-08-30 16:37:47 +0100

    created_react_vite_app
    Created a React Vite app and configured Tailwind CSS.

---
+++ tailwind-card-app/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-blue-600 text-yellow-500 p-10 rounded-4xl'>Hello Word</h1>
+    </>
+  )
+}
+
+export default App
